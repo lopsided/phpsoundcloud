b689f5ba5bb9ef0fd20f660fb47dd44593c621f5
Rob Bast <rob.bast@gmail.com>
2014-11-05 16:31:06 +0100

    Added some (c) notices

--- source/SoundCloud.php
+++ source/SoundCloud.php
@@ -1,4 +1,13 @@
-<?php namespace Alcohol;
+<?php
+
+/**
+ * (c) Rob Bast <rob.bast@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Alcohol;
 
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Message\ResponseInterface;
--- tests/IntegrationTests.php
+++ tests/IntegrationTests.php
@@ -1,4 +1,13 @@
-<?php namespace Alcohol\Tests;
+<?php
+
+/**
+ * (c) Rob Bast <rob.bast@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Alcohol\Tests;
 
 use Alcohol\SoundCloud;
 
